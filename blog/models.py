5cbcf068eefdf8c978a2b9b29e9b3124a0e8f0dd
Hassan Thabet <dev.7assan@gmail.com>
2023-02-04 01:58:56 +0200

    add article model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+# image
+# title
+# article
+# author
+# tags
+# category
+# published at
+
+
+class Blog(models.Model):
+    author = models.CharField(max_length=100)
+    image = models.CharField(max_length=150)
+    title = models.CharField(max_length=150)
+    article = models.TextField(max_length=5000)
+    published_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
