d1554f6bf9774c4b3a96a60c1f8f4e13dff701fb
Hassan Thabet <dev.7assan@gmail.com>
2023-02-04 03:47:24 +0200

    add template files

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,6 @@
-from django.urls import path , include
+from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('', views.AllBlogs),
+    path('', views.BlogList),
     path('<int:id>', views.BlogDetails),
 ]
-
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
 
-def AllBlogs(request):
-    pass
 
-def BlogDetails(request , id):
-    pass
+def BlogList(request):
+
+    all_blogs = Blog.objects.all()
+    context = {'blogs': all_blogs}
+    return render(request, 'blog/blog_list.html', context)
+
+
+def BlogDetails(request, id):
+    blog_details = Blog.objects.get(id=id)
+    context = {'blog': blog_details}
+    return render(request, 'blog/blog_details.html', context)
